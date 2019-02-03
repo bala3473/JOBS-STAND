4a33c448ec46ff9a72686e1f11bc661b3c8fcfa5
Admin <Admin@bala>
2019-02-04 00:34:31 +0530

    project-2 backend

---
+++ collaborationStore/src/main/java/emp/enq/collaborationStore/App.java
@@ -0,0 +1,13 @@
+package emp.enq.collaborationStore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
