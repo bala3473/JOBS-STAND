f87ed0350e6faa6bb94aed92c9fcdcd8732d2056
Admin <Admin@bala>
2019-02-04 12:06:12 +0530

    project-2 back end

---
+++ collaborationStore/src/main/java/emp/enq/configuration/WebAppconfiger.java
@@ -0,0 +1,11 @@
+package emp.enq.configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebAppconfiger extends WebMvcConfigurerAdapter {
+	
+	public WebAppconfiger() {
+		System.out.println("web app configuration created or instantiated");
+	}
+
+}
